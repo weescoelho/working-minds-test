d76afad988156d3154417e0402f1396f039c84b0
weescoelho <wes.coelho18@gmail.com>
2021-07-05 20:31:46 -0300

    refactor: remove :root styles

--- src/styles/global.ts
+++ src/styles/global.ts
@@ -1,14 +1,7 @@
 import { createGlobalStyle } from 'styled-components';
 
 export default createGlobalStyle`
-:root{
-  font-size:62.5%;
-}
-@media (max-width:739px){
-  :root{
-  font-size:52.5%;
-}
-}
+
   *{
     margin:0;
     padding:0;
