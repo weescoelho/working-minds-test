831b010170a7609c4edd557cb1a1f744b273471a
weescoelho <wes.coelho18@gmail.com>
2021-07-05 20:42:20 -0300

    feat: added toastprovider

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,16 @@
 import Routes from 'routes';
+import { ThemeProvider } from 'styled-components';
+import { ToastProvider } from 'react-toast-notifications';
+import light from 'styles/themes/light';
+import GlobalStyle from './styles/global';
 
 export function App(): JSX.Element {
-  return <Routes />;
+  return (
+    <ThemeProvider theme={light}>
+      <ToastProvider placement="bottom-right">
+        <GlobalStyle />
+        <Routes />
+      </ToastProvider>
+    </ThemeProvider>
+  );
 }
