32aab09f0c868fce865d8bcc9daef998d3f1f4a7
weescoelho <wes.coelho18@gmail.com>
2021-07-06 00:11:27 -0300

    refactor: added link on logo

--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -1,11 +1,19 @@
+import { Link } from 'react-router-dom';
 import styled from 'styled-components';
 
 const LogoContainer = styled.div`
-  font-size: 2.4rem;
-  font-weight: 700;
-  color: ${({ theme }) => theme.colors.secondary};
+  a {
+    font-size: 1.6rem;
+    font-weight: 700;
+    color: ${({ theme }) => theme.colors.secondary};
+    text-decoration: none;
+  }
 `;
 
 export function Logo(): JSX.Element {
-  return <LogoContainer>CRUD</LogoContainer>;
+  return (
+    <LogoContainer>
+      <Link to="/">CRUD</Link>
+    </LogoContainer>
+  );
 }
