98d0e2ae9a241d80a7c9141e16c11355535a75b0
weescoelho <wes.coelho18@gmail.com>
2021-07-05 23:55:45 -0300

    feat: created new routes for StatePage and StateForm

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,3 +1,5 @@
+import { StateForm } from 'pages/StateForm';
+import { StatePage } from 'pages/StatePage';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
 import { Home } from '../pages/Home';
@@ -6,6 +8,8 @@ const Routes: React.FC = () => (
   <Router>
     <Switch>
       <Route path="/" exact component={Home} />
+      <Route path="/cadastrar/estado" exact component={StateForm} />
+      <Route path="/estado/:id" exact component={StatePage} />
     </Switch>
   </Router>
 );
