32182f7db49f3952ba63eb4f19b9d21f301ab196
weescoelho <wes.coelho18@gmail.com>
2021-07-06 00:13:32 -0300

    feat: added new functionalities at Home page

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,5 +1,55 @@
-import React from 'react';
+import React, { useState } from 'react';
+import styled from 'styled-components';
+import { useHistory } from 'react-router-dom';
+import { api } from 'api/api';
+import { Header } from 'components/Header';
+import { City, States } from 'utils/types';
+import { Button } from '@material-ui/core';
+import { StateCard } from '../../components/StateCard';
+
+const Container = styled.div`
+  max-width: 900px;
+  margin: 0 auto;
+`;
+
+const StateGrid = styled.div`
+  display: grid;
+  grid-template-columns: 1fr 1fr 1fr;
+  column-gap: 16px;
+  row-gap: 16px;
+  margin: 64px 0;
+`;
 
 export function Home(): JSX.Element {
-  return <h1>Home page</h1>;
+  const [data, setData] = useState<States>();
+  const history = useHistory();
+
+  React.useEffect(() => {
+    const getData = async (): Promise<void> => {
+      const stateData = await api.get('/states');
+      setData(stateData.data);
+    };
+    getData();
+  }, []);
+
+  const handleCreateState = (): void => {
+    history.push('/cadastrar/estado');
+  };
+
+  return (
+    <>
+      <Header />
+      <Container>
+        <StateGrid>
+          {data &&
+            data.map(state => (
+              <StateCard key={state.id} id={state.id} stateName={state.name} />
+            ))}
+        </StateGrid>
+        <Button variant="contained" color="primary" onClick={handleCreateState}>
+          Adicionar estado
+        </Button>
+      </Container>
+    </>
+  );
 }
