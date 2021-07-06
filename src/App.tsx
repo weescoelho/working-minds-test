4f7e2ad85763fb0969db6f17d202065c8290277d
weescoelho <wes.coelho18@gmail.com>
2021-07-06 14:38:02 -0300

    refactor: themeprovider removed

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,15 @@
 import Routes from 'routes';
-import { ThemeProvider } from 'styled-components';
 import { ToastProvider } from 'react-toast-notifications';
 import { DataContextProvider } from 'contexts/DataContext';
-import light from 'styles/themes/light';
 import GlobalStyle from './styles/global';
 
 export function App(): JSX.Element {
   return (
     <ToastProvider placement="bottom-right">
-      <ThemeProvider theme={light}>
-        <DataContextProvider>
-          <GlobalStyle />
-          <Routes />
-        </DataContextProvider>
-      </ThemeProvider>
+      <DataContextProvider>
+        <GlobalStyle />
+        <Routes />
+      </DataContextProvider>
     </ToastProvider>
   );
 }
